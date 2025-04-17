a1162db537db499ef5d2996a3da2cab2cdc29e6a
Bea <ab291012@gmail.com>
2025-04-17 16:26:25 -0300

    feat: add gif redirector

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,27 @@
 "use client";
-
+import styles from "../styles/page.module.css"
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image"
+import florgif from "../public/florgif.gif"
 
 export default function Home() {
     const router = useRouter();
 
     useEffect(() => {
-        router.push("/login");
+        router.push("/");
     }, [router]);
 	
 return (
-	<p>Redirecting …</p>
+  <main className={styles.redirector}>
+  <div className={styles.text}>
+      <p className={styles.description}>Você está sendo redirecionado...</p>
+  </div>
+  <Image
+  className={styles.gif}
+  src={florgif}
+  alt="gif de flor rosa girando"
+  />
+  </main>
 );
 }
