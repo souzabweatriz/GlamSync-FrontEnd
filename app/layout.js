da313e1bb1e97be337be5aca2f943e58688898b3
Bea <ab291012@gmail.com>
2025-04-17 15:50:08 -0300

    fix: remove middleware

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,10 @@
-
 import "../styles/globals.css";
 export const metadata = {
   title: "My First NextJS App",
 };
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="pt-BR">
       <body>{children}</body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     const router = useRouter();
 
     useEffect(() => {
-        router.push("/signUp");
+        router.push("/login");
     }, [router]);
 	
 return (
