024379029e3c04d5447a75031fa6beedca3fa61b
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-04-17 15:35:20 -0300

    feat: try to fix

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     const router = useRouter();
 
     useEffect(() => {
-        router.push("/signin");
+        router.push("/signUp");
     }, [router]);
 	
 return (
---
+++ app/signUp/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import styles from "./signUp.module.css";
+
+export default function SignUp() {
+    const router = useRouter();
+
+    return (
+        <div className={styles.signUp}>
+            
+        </div>
+    )
+}
