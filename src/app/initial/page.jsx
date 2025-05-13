239ca5b0fd2b4d9cc6c5c7b81be345a03fe4fb76
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-05-13 16:21:21 -0300

    feat: add buttons

--- src/app/initial/page.jsx
+++ src/app/initial/page.jsx
@@ -1,13 +1,15 @@
 "use client"
+
 import styles from "../initial/initial.module.css";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Initial() {
     const router = useRouter();
+
     return (
         <main className={styles.fundo}>
-            <div className={styles.teste}>
+            <div className={styles.container}>
                 <h1 className={styles.title}>WELCOME!</h1>
                 <div className={styles.logo}>
                     <div className={styles.description}>
@@ -18,17 +20,17 @@ export default function Initial() {
             <div className={styles.buttons}>
                         <button
                             className={styles.button}
-                            onClick={() => router.push("/SignIn")}
+                            onClick={() => router.push("/signIn")}
                         >
                             Log In
-                            <span className={styles.icon}><Image className={styles.icon} src="/icons/GoogleIcon.png" alt="Logo do Google" width={32} height={32} /></span>
+                            <span className={styles.icon}><Image className={styles.icon} src="/icons/icon-entrar.png" alt="Logo do Google" width={32} height={32} /></span>
                         </button>
                         <button
                             className={styles.button}
-                            onClick={() => router.push("/SignUp")}
+                            onClick={() => router.push("/signUp")}
                         >
                             SignUp
-                            <span className={styles.icon}><Image className={styles.icon} src="/icons/GoogleIcon.png" alt="Logo do Google" width={32} height={32} /></span>
+                            <span className={styles.icon}><Image className={styles.icon} src="/icons/user.png" alt="Logo do Google" width={32} height={32} /></span>
                         </button>
                     </div>
             </div>
