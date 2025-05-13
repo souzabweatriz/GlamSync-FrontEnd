cf291101d43994b479b9ab40d747b0e1e9eba9ad
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-05-13 11:33:16 -0300

    refactor: layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,19 @@
 import "../styles/globals.css";
-import {Montserrat} from 'next/font/google'
-
+import { Montserrat } from "next/font/google";
 
 export const metadata = {
   title: "GlamSync",
 };
 
-
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" className={montserrat.className}>
-      <body>{children}</body>
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-BR">
       <head>
         <link rel="icon" href="/icons/favicon.ico" />
       </head>
