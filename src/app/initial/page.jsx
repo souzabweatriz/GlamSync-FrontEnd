c3bf6c69ce31b1a46baa962ce26288f9a8498071
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-05-13 15:36:14 -0300

    refactor: page initial

--- src/app/initial/page.jsx
+++ src/app/initial/page.jsx
@@ -1,15 +1,37 @@
-import styles from "../initial/initial.module.css"
+"use client"
+import styles from "../initial/initial.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function Initial(){
-    return(
-        <main>
-        <div className={styles.fundo}>
-            <h1 className={styles.title}>Welcome!</h1>
-        </div>
-        <div className={styles.logo}></div>
+export default function Initial() {
+    const router = useRouter();
+    return (
+        <main className={styles.fundo}>
+            <div className={styles.teste}>
+                <h1 className={styles.title}>WELCOME!</h1>
+                <div className={styles.logo}>
+                    <div className={styles.description}>
+                        <p>Fashion that conects</p>
+                        <p>Style that impacts</p>
+                    </div>
+                </div>
+            <div className={styles.buttons}>
+                        <button
+                            className={styles.button}
+                            onClick={() => router.push("/SignIn")}
+                        >
+                            Log In
+                            <span className={styles.icon}><Image className={styles.icon} src="/icons/GoogleIcon.png" alt="Logo do Google" width={32} height={32} /></span>
+                        </button>
+                        <button
+                            className={styles.button}
+                            onClick={() => router.push("/SignUp")}
+                        >
+                            SignUp
+                            <span className={styles.icon}><Image className={styles.icon} src="/icons/GoogleIcon.png" alt="Logo do Google" width={32} height={32} /></span>
+                        </button>
+                    </div>
+            </div>
         </main>
-
-        
     )
 }
