220b5e7a00ae9e5fe3c0b0fbb7eb01fe03b13ccd
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-05-20 21:36:19 -0300

    feat: add items page Feed

--- src/app/Feed/page.jsx
+++ src/app/Feed/page.jsx
@@ -1,26 +1,31 @@
 import styles from "./feed.module.css";
 import Image from "next/image";
+import Link from 'next/link';
+import PageFeed from "../../components/PageFeed"
 
-export default function PrincipalFeed(){
-    return(
+export default function PrincipalFeed() {
+
+    const navItems = [
+        { href: "/initial", src: "/icons/home.png", alt: "ícone de casa marrom", size: 24 },
+        { href: "/initial", src: "/icons/iconAdicionar.png", alt: "ícone de adicionar marrom", size: 27 },
+        { href: "/initial", src: "/icons/iconChat.png", alt: "ícone de chat marrom", size: 27 },
+        { href: "/initial", src: "/icons/users.png", alt: "ícone de usuário marrom", size: 27 },
+        { href: "/initial", src: "/icons/iconSino.png", alt: "ícone de sino marrom", size: 27 },
+    ];
+
+    return (
         <main className={styles.main}>
-            <header className={styles.header}>
-        <div className={styles.iconsContainer}>
-            <Image src="/icons/home.png" alt="ícone de casa marrom" width={24} height={24} />
-        </div>
-        <div className={styles.iconsContainer}>
-            <Image src="/icons/iconAdicionar.png" alt="ícone de Adicionar marrom" width={20} height={20} />
-        </div>
-        <div className={styles.iconsContainer}>
-            <Image src="/icons/iconChat.png" alt="ícone de Chat marrom" width={20} height={20} />
-        </div>
-        <div className={styles.iconsContainer}>
-            <Image src="/icons/users.png" alt="ícone de Usuário marrom" width={20} height={20} />
-        </div>
-        <div className={styles.iconsContainer}>
-            <Image src="/icons/iconSino.png" alt="ícone de Sino marrom" width={20} height={20} />
-        </div>
-        </header>
+            <div className={styles.header}>
+                {navItems.map((item, index) => (
+                    <Link href={item.href} key={index} className={styles.navLink}>
+                        <Image src={item.src} alt={item.alt} width={item.size} height={item.size} />
+                    </Link>
+                ))}
+            </div>
+            <div>
+                <Image src="/LogoGrande.png" alt="ícone de Chat marrom" width={300} height={300} />
+            </div>
+            <PageFeed />
         </main>
     )
 }
--- src/components/PageFeed.jsx
+++ src/components/PageFeed.jsx
@@ -1,42 +1,80 @@
 import React from "react";
 import styles from "../styles/PageFeed.module.css";
+import Image from "next/image";
 
-export default function Feed(){
-    return(
+export default function Feed() {
+    return (
         <div className={styles.main}>
-            <div className={styles.input}> 
-            <Image 
-            className={styles.lupa} 
-            src="../icons/lupa.png" 
-            alt="Ícone de Lupa" 
-            width={50} 
-            height={50} 
-            />
-            <input className={styles.text}
-            type="text"
-            placeholder="Search..." />
+            <div className={styles.input}>
+                <Image
+                    className={styles.lupa}
+                    src="/icons/lupa.png"
+                    alt="Ícone de Lupa"
+                    width={24}
+                    height={24}
+                />
+                <input className={styles.text} type="text" placeholder="Search..." />
             </div>
+
             <div className={styles.sections}>
-            <ul className={styles.links}>
-                <li>
-                    <Image src="../public/" alt="Imagem da seção de vestidos" />
-                    <a className={styles.list} href="/Dresses" />
-                </li>
-                <li>
-                    <Image src="../public/" alt="Imagem da seção de vestidos" />
-                    <a className={styles.list} href="/Dresses" />
-                </li>
-                <li>
-                    <Image src="../public/" alt="Imagem da seção de vestidos" />
-                    <a className={styles.list} href="/Dresses" />
-                </li>
-                <li>
-                    <Image src="../public/" alt="Imagem da seção de vestidos" />
-                    <a className={styles.list} href="/Dresses" />
-                </li>
-                
-            </ul>
+                <ul className={styles.links}>
+                    <li>
+                        <a className={styles.list} href="/Dresses">
+                            <Image
+                                src="/icons/dress.png"
+                                alt="Imagem da seção de vestidos"
+                                width={40}
+                                height={40}
+                            />
+                            <span>Dress</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.list} href="/Hats">
+                            <Image
+                                src="/icons/hat.png"
+                                alt="Imagem da seção de chapéus"
+                                width={40}
+                                height={40}
+                            />
+                            <span>Hat</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.list} href="/Shoes">
+                            <Image
+                                src="/icons/shoes.png"
+                                alt="Imagem da seção de sapatos"
+                                width={40}
+                                height={40}
+                            />
+                            <span>Shoes</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.list} href="/Makeup">
+                            <Image
+                                src="/icons/makeup.png"
+                                alt="Imagem da seção de maquiagens"
+                                width={40}
+                                height={40}
+                            />
+                            <span>Make Up</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.list} href="/Jewelry">
+                            <Image
+                                src="/icons/jewelry.png"
+                                alt="Imagem da seção de joias"
+                                width={40}
+                                height={40}
+                            />
+                            <span>Jewelry</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
-    )
+    );
 }
