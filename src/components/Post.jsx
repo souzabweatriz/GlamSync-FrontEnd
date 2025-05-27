7023060c52efee7ff6b503a533a3c0da3c5922d0
Bea <ab291012@gmail.com>
2025-05-27 11:00:21 -0300

    feat: add react-toastify by AnaSouza

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,40 @@
 "use client"
 import styles from "../styles/Post.module.css";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Post() {
 
     const [showFollowing, setFollowing] = useState(false); 
+    const [comments, setComments] = useState([]);
 
     const handleFollow = () => {
         setFollowing((prev) => !prev);
       };
 
+      useEffect(() =>{
+        const fetchComments = async () => {
+            try {
+                const { data: comments  } = await axios.get(
+                    `${process.env.NEXT_PUBLIC_API_URL}/comments`,
+                    {
+                        headers: HEADERS,   
+                    }
+                );
+                setData({comments, loading: false, current: 1, pageSize: 5});
+            } catch (error) {
+                toast.error("Erro ao carregar Comentários");
+                setData((d) => ({ ...d, loading: false}));
+            }
+        };
+        fetchComments();
+      }, []);
+
     return(
         <div className={styles.container}>
+            <ToastContainer />
         <h1 className={styles.title}>Feed</h1>
         <div className={styles.post}>
             <Image src="/user.png" alt="ícone de usuário na cor marrom" width={30} height={30} />
