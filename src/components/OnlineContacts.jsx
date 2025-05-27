3c30090fd1056ea7dd6dacfc6a374fee30c27dfb
Julianeves01 <julianvs2007@gmail.com>
2025-05-27 09:24:11 -0300

    I remade the messages page

--- src/components/OnlineContacts.jsx
+++ src/components/OnlineContacts.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import styles from "../styles/OnlineContacts.module.css";
 
 const OnlineContacts = () => {
+    const contacts = Array (5).fill('@username');
     return (
-        <div className={styles.box}>
-            <h3 className={styles.h3}>
-                Online Contacts <span className={styles.onlineDot} />
+        <div className={styles.container}>
+            <h3>
+                Online Contacts <span className={styles.onlineDot} ></span>
             </h3>
-            <div className={styles.avatares}>
-                {[...Array(5)].map ((_, i) => (
-                    <div key={i} className={styles.user}>
-                        <div className={styles.avatar}>
-                            <div className={styles.greenDot}></div>
-                        </div>
-                        <p>@username
-                        </p>
+            <div className={styles.avatars}>
+                {contacts.map((username, i) => (
+                    <div key={i} className={styles.avatar}>
+                        <div className={styles.image}></div>
+                        <span>{username}</span>
                     </div>
                 ))}
             </div>
--- src/components/RecentContacts.jsx
+++ src/components/RecentContacts.jsx
@@ -3,29 +3,37 @@ import styles from "../styles/RecentContacts.module.css";
 
 const RecentContacts = () => {
     const contacts = [
-        { msg: "Sent you a post from @cocojones", date: "24 Apr" },
-        { msg: "Quais peças de roupas você mais...", date: "15 Apr" },
-        { msg: "Comprei recentemente uma bota...", date: "31 Mar" },
-        { msg: "Onde você compra seus sapatos?", date: "2 Feb" },
-        { msg: "Hi There, Welcome to GlamSync! Sh...", date: "28 Jan" },
+        { name: "@username", msg: "Sent you a post from @cocojones", date: "24 Apr", unread: 2 },
+        { name: "@username", msg: "Quais peças de roupas você mais...", date: "15 Apr" },
+        { name: "@username", msg: "Comprei recentemente uma bota...", date: "31 Mar", unread: 1 },
+        { name: "@username", msg: "Onde você compra seus sapatos?", date: "2 Feb" },
+        { name: "@username", msg: "Hi There, Welcome to GlamSync! Sh...", date: "28 Jan" },
     ];
 
     return (
-        <div className={styles.box}>
+        <div className={styles.container}>
             <h3>Recent Contacts</h3>
-            {contacts.map((item, i) => (
-                <div key={i} className={styles.contact}>
-                    <div className={styles.avatar}></div>
-                    <div className={styles.details}>
-                        <p>@username</p>
-                        <span>{item.msg}</span>
-                    </div>
-                    <div className={styles.meta}>
-                        <p>{item.date}</p>
-                        <div className={styles.dot}></div>
-                    </div>
-                </div>
-            ))}
+            <ul className={styles.list}>
+                {contacts.map((c, i) => (
+                    <li key={i} className={styles.contact}>
+                        <div className={styles.info}>
+                            <div className={styles.avatar}></div>
+                            <div>
+                                <strong>{c.name}</strong>
+                                <p>{c.msg}</p>
+                            </div>
+                        </div>
+                        <div className={styles.right}>
+                            <span>{c.date}</span>
+                            {c.unread && (
+                                <div className={styles.badgeContainer}>
+                                    <div className={styles.badge}>{c.unread}</div>
+                                </div>
+                            )}
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
