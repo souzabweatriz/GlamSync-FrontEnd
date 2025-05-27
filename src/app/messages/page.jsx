d0fac6f9b8477261b6a92740cf92ca69c1fcbc60
Julianeves01 <julianvs2007@gmail.com>
2025-05-27 11:48:19 -0300

    finished

--- src/app/messages/page.jsx
+++ src/app/messages/page.jsx
@@ -3,25 +3,27 @@ import styles from '../../styles/messages.module.css';
 import Header from '../../components/Header';
 import OnlineContacts from '../../components/OnlineContacts';
 import RecentContacts from '../../components/RecentContacts';
-import lupaIcon from "../../../public/icons/lupa.png"
+import lupaIcon from "../../../public/icons/lupa.png";
 import Image from 'next/image';
 
 const MessagesPage = () => {
     return (
-        <div className={styles.container}>
-            <Header />
-            <div className={styles.header}>
-                <h2>Messages</h2>
-                <div className={styles.search}>
-                <input type="text" placeholder="Search..." />
-          <button>
-            <Image src={lupaIcon} alt="Search" width={20} height={20} />
-            </button>
+        <div className={styles.body}>
+            <div className={styles.container}>
+                <Header />
+                <div className={styles.header}>
+                    <h2>Messages</h2>
+                    <div className={styles.search}>
+                        <input type="text" placeholder="Search..." />
+                        <button>
+                            <Image src={lupaIcon} alt="Search" width={20} height={20} />
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.grid}>
-                <OnlineContacts />
-                <RecentContacts />
+                <div className={styles.grid}>
+                    <OnlineContacts />
+                    <RecentContacts />
+                </div>
             </div>
         </div>
     );
--- src/components/OnlineContacts.jsx
+++ src/components/OnlineContacts.jsx
@@ -2,21 +2,35 @@ import React from "react";
 import styles from "../styles/OnlineContacts.module.css";
 
 const OnlineContacts = () => {
-    const contacts = Array (5).fill('@username');
+    const contacts = [
+        { username: "@username", image: "/icons/ongrayuser-icon.png" },
+        { username: "@username", image: "/icons/ongrayuser-icon.png" },
+        { username: "@username", image: "/icons/ongrayuser-icon.png" },
+        { username: "@username", image: "/icons/ongrayuser-icon.png"},
+        { username: "@username", image: "/icons/ongrayuser-icon.png" },
+    ];
+
     return (
         <div className={styles.container}>
             <h3>
-                Online Contacts <span className={styles.onlineDot} ></span>
+                Online Contacts <span className={styles.onlineDot}></span>
             </h3>
             <div className={styles.avatars}>
-                {contacts.map((username, i) => (
+                {contacts.map((contact, i) => (
                     <div key={i} className={styles.avatar}>
-                        <div className={styles.image}></div>
-                        <span>{username}</span>
+                        <img
+                            src={contact.image}
+                            alt={`Avatar of ${contact.username}`}
+                            width={50}
+                            height={50}
+                            className={styles.image}
+                        />
+                        <span>{contact.username}</span>
                     </div>
                 ))}
             </div>
         </div>
     );
 };
+
 export default OnlineContacts;
