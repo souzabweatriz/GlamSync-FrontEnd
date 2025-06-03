122e46b59e6b16c29c2b2c7f5bfc83fb6e501b3c
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-06-03 09:56:32 -0300

    refactor: posts at page Shoes by AnaSouza

--- src/app/Shoes/page.jsx
+++ src/app/Shoes/page.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import styles from "./Shoes.module.css"
-import Header from "../../components/Header"
-import Post from "../../components/Post"
-export default function Shoes(){
-    return(
-        <div className={styles.container}>
-            <Header 
+import styles from "./Shoes.module.css";
+import Header from "../../components/Header";
+import Post from "../../components/Post";
+export default function Shoes() {
+    return (
+        <div>
+            <div className={styles.header}>
+                <Header
                     ImageProps={{
                         src: "/flower.png",
                         alt: "icon de voltar",
                         width: 50,
                         height: 50,
-                    }}  
-            />
-            <Post title={"Shoes"} rota={`${process.env.NEXT_PUBLIC_API_URL}posts`}/>
+                    }}
+                />
+            </div>
+            <div className={styles.container}>
+                <Post
+                    className={styles.post}
+                    title={"Shoes"}
+                    rota={`${process.env.NEXT_PUBLIC_API_URL}posts`}
+                />
+            </div>
         </div>
-    )
+    );
 }
