7d231ddd3887728f5fff9f026bb4ed4efb3f2869
EvelynOliveira1200 <evelynoliveira.tds1.senai@gmail.com>
2025-06-05 11:02:30 -0300

    finished page

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Header from "../../components/Header";
+import chatHeader from "../../styles/chatHeader.module.css";
 import styles from "../../styles/chats.module.css";
 import ChatMessage from "../../components/ChatMessage";
 import ChatInput from "../../components/ChatInput";
+import Image from "next/image";
 
 
 export default function ChatPage() {
     return (
         <div className={styles["chat-gb"]}>
             <Header />
+            <div className={chatHeader.top}>
+                <button className={chatHeader.back}>
+                    <Image src="/icons/back-icon.png" alt="Voltar" width={39} height={39} />
+                </button>
+                <span className={chatHeader.user}>
+                    <Image src="/icons/users.png" alt="Usuário" width={24} height={24} className={chatHeader.icon} />
+                    @username
+                </span>
+            </div>
             <div className={styles["chat-body"]}>
                 <ChatMessage text="Ei! Você viu o novo Work Shop que a Dolce Gabbana está fazendo agora? Tá muito legal!" time="16:00" />
                 <ChatMessage
@@ -17,14 +28,14 @@ export default function ChatPage() {
                     post={{
                         text: "Olha só esse post que a @cocojones acabou de fazer!"
                     }}
-                    time="17:01"
+                    time="17:00"
                 />
                 <ChatMessage
                     post={{
                         user: "@cocojones",
-                        img: "/media/cocoJones.jpg"
+                        img: "/media/COCOJONES.png"
                     }}
-                    time="17:02"
+                    time="17:01"
                 />
             </div>
             <ChatInput />
