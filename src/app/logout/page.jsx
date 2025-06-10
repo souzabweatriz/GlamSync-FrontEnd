6abcd0dd53c8675a69ac1e4043f1794afecc9c25
Julianeves01 <julianvs2007@gmail.com>
2025-06-10 09:13:36 -0300

    finished page Logout

--- src/app/logout/page.jsx
+++ src/app/logout/page.jsx
@@ -1,14 +1,27 @@
-import Link from 'next/link';
+"use client";
+
 import styles from './logout.module.css';
+import Header from "../../components/Header";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Logout() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            router.push('/initial');
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [router]);
+
     return (
-        <div className={styles.logoutContainer}>
-            <h1 className={styles.title}>Você saiu da sua conta!</h1>
-            <p className={styles.message}>Esperamos te ver em breve!</p>
-            <Link href="/initial">
-                <button className={styles.button}>Voltar para a página inicial</button>
-            </Link>
-        </div>
+        <>
+            <Header />
+            <div className={styles.logoutContainer}>
+                <h1 className={styles.title}>Você saiu da sua conta!</h1>
+                <p className={styles.message}>Esperamos te ver em breve!</p>
+            </div>
+        </>
     );
 }
